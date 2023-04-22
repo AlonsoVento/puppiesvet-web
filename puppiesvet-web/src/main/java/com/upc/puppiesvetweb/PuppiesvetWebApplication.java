9caf4d04a9fb1492637cd0c598bd270e07b9d912
Alonso <alonso_vento@outlook.com>
2023-04-21 22:30:42 -0500

    Initial commit - Creación de Entorno

---
+++ puppiesvet-web/src/main/java/com/upc/puppiesvetweb/PuppiesvetWebApplication.java
@@ -0,0 +1,13 @@
+package com.upc.puppiesvetweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppiesvetWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppiesvetWebApplication.class, args);
+	}
+
+}
